d2fe85c9b30361e3611788df0cdb8695af5342f4
Arman <arman.zohrabyan.96@gmail.com>
2020-01-15 01:25:41 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+// задача №1
+// время выполнения зависит от количества эдементов массива. линейная.
+// Потратил 10 минут.
+
+const array = [
+  '1.5',
+  '3',
+  '6',
+  '1.5'
+]
+
+
+const t0 = performance.now();
+const getPercents = arr => {
+  const sum = arr.reduce((calc, numb) => calc + +numb, 0);
+  return arr.map(numb => (numb*100/sum).toFixed(3))
+}
+const t1 = performance.now();
+console.log("Call to doWork took " + (t1 - t0) + " milliseconds.")
+
+
+getPercents(array);
+
